a35ee8e63f8952702ba761d31eee10f1e2870eb7
Daniel Posch <daniel.posch@aau.at>
2013-06-10 17:38:06 +0200

    first commit(s) to initialise the git-repository.

---
+++ ccns.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= qt
+
+CONFIG += warn_on console debug
+DESTDIR = bin
+OBJECTS_DIR = build
+
+HEADERS = 
+
+SOURCES = src/main.cpp \
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+/*!
+  This program prints Hello world
+*/
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
